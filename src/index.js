59cbca34d93cebc772a9d5e2d8901d46cd674b2d
Bruno Terragno <bruno.terragno@gmail.com>
2018-02-06 00:45:21 -0200

    Added redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import Router, { history } from './router';
 import registerServiceWorker from './registerServiceWorker';
 
 const middlewares = applyMiddleware(ReduxThunk, routerMiddleware(history));
 
-const store = createStore(reducers, middlewares);
+const store = createStore(reducers, composeWithDevTools(middlewares));
 
 ReactDOM.render(<Router store={store} />, document.getElementById('root'));
 
